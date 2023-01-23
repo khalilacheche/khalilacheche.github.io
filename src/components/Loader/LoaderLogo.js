ef2b5387dfbd43e614662b03e563df36b3af4d32
Khalil Acheche <khalil.acheche@epfl.ch>
2023-01-23 22:25:14 +0100

    upload metadata

--- src/components/Loader/LoaderLogo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./LoaderLogo.css";
-import logo from "../../assets/logo/logo-01.png";
-class LogoLoader extends React.Component {
-  render() {
-    return <img src={logo} alt="logo" id="logo" />;
-  }
-}
-export default LogoLoader;
